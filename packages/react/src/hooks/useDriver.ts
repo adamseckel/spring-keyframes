677b89f4173e67d8d788ed99f803d7f7ddc35acf
Adam Seckel <adam.seckel@gmail.com>
2021-05-30 14:22:07 +0100

    Fix removal

--- packages/react/src/hooks/useDriver.ts
+++ packages/react/src/hooks/useDriver.ts
@@ -1,7 +1,7 @@
 import { KeyframesContext } from "../components/Keyframes"
 import { Driver } from "../Driver"
 import { useConstant } from "./useConstant"
-import { useLayoutEffect, useEffect, useContext, useCallback } from "react"
+import { useLayoutEffect, useEffect, useContext, useRef } from "react"
 import { usePresence } from "framer-motion"
 
 const createAnimation = (ref: React.RefObject<HTMLElement>, invertedRef?: React.RefObject<HTMLElement>) => (
@@ -20,31 +20,38 @@ export function useDriver(
   invertedRef?: React.RefObject<HTMLElement>
 ): Driver {
   const keyframes = useContext(KeyframesContext)
-  const presence = usePresence()
+  const [isPresent, safeToRemove] = usePresence()
   const driver = useConstant(() => new Driver(ref, keyframes, createAnimation(ref, invertedRef)))
+  const remove = useRef(safeToRemove)
 
-  const onAnimationEnd = useCallback(
-    ({ animationName }: { animationName: string }) => {
-      if (!ref.current) return
-      if (animationName !== driver.animationName) return
+  const onAnimationEnd = ({ animationName }: { animationName: string }) => {
+    if (!ref.current) return
+    if (animationName !== driver.animationName) return
 
-      ref.current.style.animation = ""
+    ref.current.style.animation = ""
 
-      driver.reset()
+    driver.reset()
 
-      if (driver.inverted && invertedRef?.current) invertedRef.current.style.animation = ""
-      if (callback) callback()
-
-      const [isPresent, safeToRemove] = presence
-      if (isPresent === false && safeToRemove) safeToRemove()
-    },
-    [driver]
-  )
+    if (driver.inverted && invertedRef?.current) invertedRef.current.style.animation = ""
+    if (callback) callback()
+    remove.current?.()
+  }
 
   useEffect(() => {
     ref.current?.addEventListener("animationend", onAnimationEnd)
-    return () => ref.current?.removeEventListener("animationend", onAnimationEnd)
+    return () => {
+      ref.current?.removeEventListener("animationend", onAnimationEnd)
+      remove.current?.()
+    }
   }, [])
+
+  useEffect(() => {
+    if (isPresent === false && safeToRemove) {
+      remove.current = safeToRemove
+      return
+    }
+    remove.current = undefined
+  }, [isPresent, safeToRemove])
 
   useLayoutEffect(driver.init, [])
 
