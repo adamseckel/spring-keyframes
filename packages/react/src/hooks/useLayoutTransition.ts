b0063aad9b61615c716e34010603e3e6bd699e56
Adam Seckel <adam.seckel@gmail.com>
2021-05-30 18:04:08 +0100

    wip

--- packages/react/src/hooks/useLayoutTransition.ts
+++ packages/react/src/hooks/useLayoutTransition.ts
@@ -12,6 +12,8 @@ export type Layout = {
   right: number
   height: number
   width: number
+  deltaX: number
+  deltaY: number
 }
 
 function rect(ref: React.RefObject<HTMLElement>) {
@@ -50,12 +52,34 @@ function getTransformDistortion(distortion?: Frame): Identity {
 
 export interface Props {
   layout?: boolean
+  id?: string
+}
+
+const clamp = (min: number, max: number, v: number) => {
+  return Math.min(Math.max(v, min), max)
+}
+const clampProgress = (v: number) => clamp(0, 1, v)
+const mix = (from: number, to: number, progress: number) => -progress * from + progress * to + from
+const progress = (from: number, to: number, value: number) => {
+  var toFromDifference = to - from
+  return toFromDifference === 0 ? 1 : (value - from) / toFromDifference
+}
+function calcOrigin(min: number, max: number, length: number, tmin: number, tmax: number, tlength: number): number {
+  let origin = 0.5
+
+  if (tlength > length) {
+    origin = progress(tmin, tmax - length, min)
+  } else if (length > tlength) {
+    origin = progress(min, max - tlength, tmin)
+  }
+
+  return clampProgress(origin)
 }
 
 export const useLayoutTransition = (
   driver: Driver,
   ref: React.RefObject<HTMLElement>,
-  { layout }: Props,
+  { layout, id }: Props,
   invertedRef: React.RefObject<HTMLElement>,
   options?: Options
 ) => {
@@ -65,6 +89,7 @@ export const useLayoutTransition = (
     if (!ref.current || !layout) return
 
     const { top, left, right, bottom } = rect(ref)
+    const parent = rect({ current: ref.current.parentElement })
     const offset = getTransformDistortion(driver.targetFrame)
 
     const { from: current } = driver.resolveValues({ base: identity })
@@ -78,6 +103,8 @@ export const useLayoutTransition = (
       right: right - x + window.scrollX,
       height: (bottom - top) / scaleY,
       width: (right - left) / scaleX,
+      deltaX: left - parent.left,
+      deltaY: top - parent.top,
     }
 
     if (lastRect.current === null) {
@@ -96,21 +123,38 @@ export const useLayoutTransition = (
 
     if (!hasRectChanged) return
 
+    const originX = calcOrigin(oldRect.left, oldRect.right, oldRect.width, newRect.left, newRect.right, newRect.width)
+    const originY = calcOrigin(oldRect.top, oldRect.bottom, oldRect.height, newRect.top, newRect.bottom, newRect.height)
+    const transformOrigin = `${originX * 100}% ${originY * 100}% 0`
+
     lastRect.current = { ...newRect }
     const flippedFrom = {
-      x: (oldRect.right - newRect.right + oldRect.left - newRect.left) / 2 + x,
-      y: (oldRect.bottom - newRect.bottom + oldRect.top - newRect.top) / 2 + y,
+      x: (mix(newRect.left, newRect.right, originX) - mix(oldRect.left, oldRect.right, originX)) * -1 + x,
+      y: (mix(newRect.top, newRect.bottom, originY) - mix(oldRect.top, oldRect.bottom, originY)) * -1 + y,
       scaleX: (oldRect.width * scaleX) / newRect.width,
       scaleY: (oldRect.height * scaleY) / newRect.height,
+      transformOrigin,
     }
+
+    if (id === "X" || id === "A")
+      console.log(id, {
+        flippedFrom,
+        x,
+        y,
+        oldRect,
+        newRect,
+        parent,
+        transformOrigin,
+      })
 
     const invertedDistortion = getTransformDistortion(createComputedFrame(identity, invertedRef))
     const invertedAnimation = {
       from: {
         scaleX: invertedDistortion.scaleX,
         scaleY: invertedDistortion.scaleY,
+        transformOrigin,
       },
-      to: { scaleX: offset.scaleX, scaleY: offset.scaleY },
+      to: { scaleX: offset.scaleX, scaleY: offset.scaleY, transformOrigin },
     }
 
     driver.animate({
@@ -120,6 +164,7 @@ export const useLayoutTransition = (
         y: identity.y + offset.y,
         scaleX: identity.scaleX * offset.scaleX,
         scaleY: identity.scaleY * offset.scaleY,
+        transformOrigin,
       },
       from: flippedFrom,
       invertedAnimation,
